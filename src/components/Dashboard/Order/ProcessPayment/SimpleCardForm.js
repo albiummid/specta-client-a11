e2e583dc629bd07f495b1776d6bca1c689bd031f
albiummid <albiummid@gmail.com>
2021-10-14 00:00:36 +0600

    payment info added

--- src/components/Dashboard/Order/ProcessPayment/SimpleCardForm.js
+++ src/components/Dashboard/Order/ProcessPayment/SimpleCardForm.js
@@ -26,6 +26,7 @@ const SimplecardForm = ({ handlePaymentSuccess }) => {
         if (error) {
             setPaymentError(error.message);
             setPaymentSuccess(null);
+                toast.error('Payment was unsuccessfull!')
         } else {
             setPaymentSuccess(paymentMethod.id);
             setPaymentError(null);
@@ -42,9 +43,20 @@ const SimplecardForm = ({ handlePaymentSuccess }) => {
                     reverseOrder={false}
                 />
                 <CardElement />
-                {paymentError &&
-                    toast.error('Payment was unsuccessfull!')
-                }
+                <div style={{display:'flex',flexDirection:'column',gap:'5px',marginTop:'15px'}}>
+                <h5>
+                    CARD: 4242 4242 4242 4242
+                </h5>
+                <h5>
+                    MM/YY: 04/30
+                </h5>
+                <h5>
+                    CVC: 424
+                </h5>
+                <h5>
+                    ZIP: 1344
+                </h5>
+               </div>
                 <button className="brand-btn" type="submit" disabled={!stripe}>
                     Order !
                 </button>
